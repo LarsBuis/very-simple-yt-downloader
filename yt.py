1b49946c3773fe7685415e95cb26df9a32c79152
Lars Buis <98239777+LarsBuis@users.noreply.github.com>
2022-01-31 19:13:05 +0100

    Add files via upload

---
+++ yt.py
@@ -0,0 +1,67 @@
+from pytube import YouTube
+import os
+#ask for the link from user
+while True:
+    print("""
+     __   _____  _   _ _____ _   _ ____  _____   ____  _     
+     \ \ / / _ \| | | |_   _| | | | __ )| ____| |  _ \| |    
+      \ V / | | | | | | | | | | | |  _ \|  _|   | | | | |    
+       | || |_| | |_| | | | | |_| | |_) | |___  | |_| | |___ 
+       |_| \___/ \___/  |_|  \___/|____/|_____| |____/|_____| made by L. Buis
+                                                                                    
+    """)
+
+
+    def audio():
+        # extract only audio
+        video = yt.streams.filter(only_audio=True).first()
+    
+        # check for destination to save file
+        print("Enter the destination (leave blank for current directory)")
+        destination = str(input(">> ")) or '.'
+    
+        # download the file
+        out_file = video.download(output_path=destination)
+    
+        # save the file
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+    
+        # result of success
+        print(yt.title + " has been successfully downloaded.")
+        return
+
+    def video():
+        ys = yt.streams.filter(file_extension='mp4').get_highest_resolution()
+        print("Enter the destination (leave blank for current directory)")
+        destination = str(input(">> ")) or '.'
+        out_file = ys.download(output_path=destination)
+        #ys.download()
+        print("Download completed!!")
+        return 
+
+    link = input("Enter the link of YouTube video you want to download:  ")
+    yt = YouTube(link)
+    print("Title: ",yt.title)
+    a_or_v = input("do you want video or audio?: ")
+
+    if a_or_v == 'audio':
+        audio()
+    elif a_or_v == 'video':
+        video()
+    elif a_or_v != 'audio' or 'video':
+        print('try again')
+
+
+
+
+
+
+    #print("Number of views: ",yt.views)
+    #print("Length of video: ",yt.length)
+    #print("Rating of video: ",yt.rating)
+    #Getting the highest resolution possible
+    #$ys = yt.streams.get_highest_resolution()
+
+    #Starting download
